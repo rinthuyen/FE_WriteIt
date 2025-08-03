f1055dfa0acad82997923d8d5e76a19118ad5f91
Rin <tuannmctk42@gmail.com>
2025-08-03 21:36:38 +0700

    install ui lib primeng and generate component login

---
+++ write-it/src/app/modules/auth/login-component/login-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-login-component',
+  imports: [ButtonModule],
+  templateUrl: './login-component.html',
+  styleUrl: './login-component.scss'
+})
+export class LoginComponent {
+    
+}
